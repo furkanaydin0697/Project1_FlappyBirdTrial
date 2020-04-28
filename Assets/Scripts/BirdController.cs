623771fec42ed49b71f63cc32e7d8860f615d2d8
Furkan Aydın <furkan_106_06@hotmail.com>
2020-04-28 16:37:33 +0300

    Flappybird benzeri yapılmış çalışma oyunu.

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdController : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float forceMagnitude = 3.0f;
+    private Rigidbody2D rb2D;
+
+    public GameObject Canvas;
+    public Text BestScore;
+    public Text Score;
+
+    public int point;
+
+    public AudioClip[] Sounds;
+
+    private void Start()
+    {
+        Canvas.SetActive(false);
+        Time.timeScale = 1;
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        point = 0;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb2D.AddForce(Vector2.up * forceMagnitude, ForceMode2D.Impulse);
+            GetComponent<AudioSource>().PlayOneShot(Sounds[2], 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D touch)
+    {
+        if (touch.gameObject.tag == "Trigger")
+        {
+            //Debug.Log("Gectin");
+            //touch.gameObject.transform.parent.gameObject.transform.parent.gameObject.GetComponent<Move>().IsTrigger = true;
+            touch.gameObject.transform.parent.root.gameObject.GetComponent<MovementController>().IsTrigger = true;
+            point++;
+            GetComponent<AudioSource>().PlayOneShot(Sounds[1], 1);
+        }
+        if (touch.gameObject.tag == "Dead")
+        {
+            //Debug.Log("Oldun");
+            GameEnd();
+        }
+    }
+
+
+    void GameEnd()
+    {
+        Time.timeScale = 0;
+        GetComponent<AudioSource>().PlayOneShot(Sounds[0], 1);
+        Canvas.SetActive(true);
+        if (point > PlayerPrefs.GetInt("BestScore"))
+        {
+            PlayerPrefs.SetInt("BestScore", point);
+        }
+        Score.text = point.ToString();
+        BestScore.text = PlayerPrefs.GetInt("BestScore").ToString();
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel("Level1");
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public bool IsTrigger;
+    public GameObject Columns;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsTrigger = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsTrigger)
+        {
+            Invoke("MoveForward", 3f);
+            IsTrigger = false;
+        }
+    }
+
+    public void MoveForward()
+    {
+        transform.position = transform.position + new Vector3(22.8f, 0, 0);
+        //-0.55 1.85 arasinda
+        float yPos = Random.Range(-0.30f, 2.35f);
+        Columns.transform.localPosition = new Vector3(0, yPos, 0);
+    }
+}
